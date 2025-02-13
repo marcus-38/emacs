481d14b925c29bf9dcc466355fd9e3124550a8f2
Marcus Kronlid <marcus@r38.se>
2025-02-13 11:31:48 +0100

    fixed appearance switching to follow the system

--- README.el
+++ README.el
@@ -1,2 +1,10 @@
 (tool-bar-mode -1)
 (scroll-bar-mode -1)
+
+(defun my/apply-theme (appearance)
+  "Load theme, taking current system APPEARANCE into consideration."
+  (mapc #'disable-theme custom-enabled-themes)
+  (pcase appearance
+    ('light (load-theme 'tango t))
+    ('dark (load-theme 'tango-dark t))))
+(add-hook 'ns-system-appearance-change-functions #'my/apply-theme)
